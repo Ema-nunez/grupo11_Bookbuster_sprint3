dba021833f93ee8ceab520371c3df8e0a615dae8
jhonatannunez <jhonatanchipana4@gmail.com>
2021-12-09 05:51:29 -0300

    Agregado de estilos a las nuevas vistas

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,16 +1,83 @@
 const objetos = [
     {
-        nombre : 'dragon ball',
+        nombre : 'Spy x Family 02',
         editoria : 'ivrea',
-        precio : 1231,
-        img : 'images/goku.png'
+        precio : 550,
+        img : 'images/spyxfamily02.jpg'
 
     },
 
     {
-        nombre : "Stone Ocean",
-        editoria : "Panini",
-        precio: 5000
+        nombre : "Metal Gear Solid",
+        editoria : "Ivrea",
+        precio: 600,
+        img : 'images/productoOferta1.jpg'
+    },
+    
+    {
+        nombre: "Death Note",
+        editoria : 'Ivrea',
+        precio : 450,
+        img : 'images/producto9.jpg'
+    },
+
+    {
+        nombre : 'Spy x Family 03',
+        editoria: 'Ivrea',
+        precio : 300,
+        img : 'images/spyxfamily03.jpg'
+    },
+    {
+        nombre: 'Teenage Mutant Ninja Tourtles',
+        editoria : 'Ivrea',
+        precio: 350,
+        img : 'images/productoOferta2.jpg'
+    },
+
+    {
+        nombre : 'Resident Evil',
+        editoria : 'Ivrea',
+        precio : 400,
+        img : 'images/producto11.jpg'
+    },
+
+    {
+        nombre: 'Spy x Family 06' ,
+        editoria: 'Ivrea',
+        precio: 450 ,
+        img : 'images/spyxfamily06.jpg'
+    },
+    {
+        nombre : 'The Amazing Spider-Man' ,
+        editoria : 'Marvel' ,
+        precio : 550 ,
+        img : 'images/productoOferta3.jpg'
+    },
+
+    {
+        nombre : 'My hero Academia',
+        editoria : 'Ivrea',
+        precio : 450,
+        img: '/images/producto14.jpg'
+    },
+    {
+        nombre : 'Spy x Family 07',
+        editoria : 'Ivrea',
+        precio: 400,
+        img: 'images/spyxfamily07.jpg'
+    },
+    {
+        nombre: 'Justice League',
+        editoria : 'Dc Comics',
+        precio : 500,
+        img : 'images/productoOferta4.jpg'
+    },
+    {
+        nombre : 'Tokyo Revengers',
+        editoria :'Ivrea',
+        precio : 600,
+        img: 'images/producto16.jpg'
+
     }
 
     
@@ -19,7 +86,6 @@ const objetos = [
 const mainController = {
     index : (req,res)=>{
         res.render('products/index', {mangas : objetos})
-        objetos.push({nombre:'punpun', editoria:'ivrea',precio : 1400})
     }
 }
 
